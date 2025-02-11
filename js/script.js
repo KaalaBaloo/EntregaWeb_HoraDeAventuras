f43c0a2792c7e214fa317d82f32b7500dc6beb59
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2025-02-11 16:38:56 +0100

    add character selector js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-$(document).ready(function()
-{
-    console.log("Running");
+$(document).ready(function() {
+    $('#gallery img').mouseenter(function() {
+        // Get data attributes from the hovered image
+        let name = $(this).data('name');
+        let image = $(this).data('image');
+        let details = $(this).data('details');
+        let stats = $(this).data('stats');
+
+        // Update the character display section
+        $('#selected h2').text(name);
+        $('#selected #info img').attr('src', image);
+        $('#selected #info p').text(details);
+        $('#selected #stats p').text(stats);
+    });
 });
